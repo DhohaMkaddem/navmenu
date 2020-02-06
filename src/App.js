df220d081249e032d005b4503316b5a3f8e97036
unknown <dohamkaddem@gmail.com>
2020-02-06 17:35:09 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./Components/Header";
+import "./App.css";
+
+const home = {
+  id: 1,
+  title: "Home",
+  selected: false
+};
+const services = {
+  id: 2,
+  title: "Services",
+  selected: true,
+  dropDownArr: [
+    { id: 1, title: "item1" },
+    { id: 2, title: "item2" },
+    { id: 3, title: "item3" }
+  ]
+};
+const about = {
+  id: 3,
+  title: "About",
+  selected: false
+};
+const navMenu = [home, services, about];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header linkListe={navMenu} />
     </div>
   );
 }
